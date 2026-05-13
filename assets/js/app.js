eb3946ede3e908e80ec9789d90eeb210d51c8511
ppp3ppj <ppp.3ppj@gmail.com>
2026-05-13 21:38:46 +0700

    Add example hook

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,12 +26,13 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "topbar"
+import Hooks from "./hooks"
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: {_csrf_token: csrfToken},
-  hooks: {},
+  hooks: Hooks,
 })
 
 // Show progress bar on live navigation and form submits
